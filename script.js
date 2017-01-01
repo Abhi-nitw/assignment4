3bbd912fbe6e12b6f35d67752c33461830f9190c
abhi-nitw <abhishek.singh.hbn@gmail.com>
2017-01-02 02:48:44 +0530

    initializing

---
+++ script.js
@@ -0,0 +1,11 @@
+var names = ["Abhishek", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+  var firstLetter =names[i].charAt(0);
+  var result = firstLetter.toLowerCase();
+  if (result=="j") {
+    byeSpeaker.speak(names[i]);
+  } 
+  else {
+    helloSpeaker.speak(names[i]);
+  }
+}
